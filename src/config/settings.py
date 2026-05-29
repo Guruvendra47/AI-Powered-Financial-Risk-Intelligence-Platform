cd6877c0c2d6ad7466753a3eb57188c8b2a7e33f
Guruvendra47 <Guruvendra47@gmail.com>
2026-05-29 14:37:09 -0400

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -3,7 +3,10 @@ import os
 
 load_dotenv()
 
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_REGION = os.getenv("AWS_REGION")
+AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
 
 SNOWFLAKE_ACCOUNT = os.getenv("SNOWFLAKE_ACCOUNT")
 SNOWFLAKE_USER = os.getenv("SNOWFLAKE_USER")
