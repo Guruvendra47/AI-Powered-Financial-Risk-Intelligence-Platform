20b1f880f00971e8e3f8e6903e568e7cd4538109
Guruvendra47 <Guruvendra47@gmail.com>
2026-05-29 13:22:01 -0400

    Update null_check.py

--- src/validation/rules/null_check.py
+++ src/validation/rules/null_check.py
@@ -1,1 +1,16 @@
+from src.utils.logger import get_logger
 
+logger = get_logger()
+
+def check_nulls(df):
+
+    null_counts = df.isnull().sum()
+
+    null_columns = null_counts[null_counts > 0]
+
+    if len(null_columns) > 0:
+        logger.warning(
+            f"Columns with null values:\n{null_columns}"
+        )
+
+    return null_columns
