0c70253fdc308d72a1250c2002ae33ed7529604d
Guruvendra47 <Guruvendra47@gmail.com>
2026-05-29 08:01:51 -0400

    Update complaint_ingestion.py

--- src/ingestion/complaints/complaint_ingestion.py
+++ src/ingestion/complaints/complaint_ingestion.py
@@ -1,1 +1,10 @@
+from src.utils.logger import get_logger
 
+logger = get_logger()
+
+def main():
+    logger.info("Complaint ingestion pipeline started")
+    logger.info("Pipeline execution successful")
+
+if __name__ == "__main__":
+    main()
