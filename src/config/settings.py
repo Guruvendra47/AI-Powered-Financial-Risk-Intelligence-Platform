654cdedbbb04f28d24e08d62f1a096ba42f88b80
Guruvendra47 <Guruvendra47@gmail.com>
2026-05-29 07:59:56 -0400

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,1 +1,12 @@
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+AWS_REGION = os.getenv("AWS_REGION")
+
+SNOWFLAKE_ACCOUNT = os.getenv("SNOWFLAKE_ACCOUNT")
+SNOWFLAKE_USER = os.getenv("SNOWFLAKE_USER")
+SNOWFLAKE_PASSWORD = os.getenv("SNOWFLAKE_PASSWORD")
+
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "llama3")
