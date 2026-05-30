29f03503363b7884f68f63c867a121df1755a0ad
Guruvendra47 <Guruvendra47@gmail.com>
2026-05-30 03:37:56 -0400

    Create connection.py

---
+++ src/cloud/snowflake/connection.py
@@ -0,0 +1,23 @@
+import snowflake.connector
+
+from src.config.settings import (
+    SNOWFLAKE_ACCOUNT,
+    SNOWFLAKE_USER,
+    SNOWFLAKE_PASSWORD,
+    SNOWFLAKE_WAREHOUSE,
+    SNOWFLAKE_DATABASE,
+    SNOWFLAKE_SCHEMA
+)
+
+def get_connection():
+
+    conn = snowflake.connector.connect(
+        account=SNOWFLAKE_ACCOUNT,
+        user=SNOWFLAKE_USER,
+        password=SNOWFLAKE_PASSWORD,
+        warehouse=SNOWFLAKE_WAREHOUSE,
+        database=SNOWFLAKE_DATABASE,
+        schema=SNOWFLAKE_SCHEMA
+    )
+
+    return conn
