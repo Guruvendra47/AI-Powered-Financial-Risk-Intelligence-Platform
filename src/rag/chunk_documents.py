4a09f2370256f82553c0d71947d6d31c950c5bd8
Guruvendra47 <Guruvendra47@gmail.com>
2026-05-30 13:02:00 -0400

    Create chunk_documents.py

---
+++ src/rag/chunk_documents.py
@@ -0,0 +1,28 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+from src.rag.document_loader import load_documents
+
+
+def chunk_documents():
+
+    documents = load_documents()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks = text_splitter.split_documents(
+        documents
+    )
+
+    return chunks
+
+
+if __name__ == "__main__":
+
+    chunks = chunk_documents()
+
+    print(
+        f"Chunks Created: {len(chunks)}"
+    )
