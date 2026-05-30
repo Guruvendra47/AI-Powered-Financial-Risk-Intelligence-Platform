cfb3d7bfaae55b33dd9ed1bca2ec998843f7ee89
Guruvendra47 <Guruvendra47@gmail.com>
2026-05-30 03:38:58 -0400

    Create test_connection.py

---
+++ src/cloud/snowflake/test_connection.py
@@ -0,0 +1,23 @@
+from src.cloud.snowflake.connection import get_connection
+
+def main():
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "SELECT CURRENT_VERSION()"
+    )
+
+    result = cursor.fetchone()
+
+    print(
+        f"Snowflake Version: {result[0]}"
+    )
+
+    cursor.close()
+    conn.close()
+
+if __name__ == "__main__":
+    main()
