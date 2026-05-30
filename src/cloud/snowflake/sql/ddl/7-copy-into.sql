8caf8014d7ebbcffc96d4c68c1019c4727ce79f8
Guruvendra47 <Guruvendra47@gmail.com>
2026-05-30 07:32:06 -0400

    Create 7-copy-into.sql

---
+++ src/cloud/snowflake/sql/ddl/7-copy-into.sql
@@ -0,0 +1,23 @@
+
+-- Load Data from S3
+-- CSV file
+COPY INTO raw_complaints
+FROM @risk_complaints_stage
+FILE_FORMAT = complaints_csv_format
+PATTERN = '.*\\.csv'
+-- adding continue which skip bad rows check before adding this
+ON_ERROR = CONTINUE;
+
+--or--
+
+-- PARQUET file
+COPY INTO raw_stock_data
+FROM @realtime_stage
+FILE_FORMAT = complaints_parquet_format
+PATTERN = '.*\\.parquet'
+MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE;
+
+-- Verify the Load
+SELECT 
+  COUNT(*) 
+FROM raw_complaints;
