667a64b1b32349092ce8367e397b0963b8e7fadc
Guruvendra47 <Guruvendra47@gmail.com>
2026-05-30 13:22:56 -0400

    Update 3-create-embeddings.py

--- src/rag/3-create-embeddings.py
+++ src/rag/3-create-embeddings.py
@@ -1,1 +1,34 @@
+from dotenv import load_dotenv
 
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+
+from src.rag.chunk_documents import chunk_documents
+
+load_dotenv()
+
+
+def create_vector_store():
+
+    chunks = chunk_documents()
+
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-3-small"
+    )
+
+    vector_store = Chroma.from_documents(
+        documents=chunks,
+        embedding=embeddings,
+        persist_directory="vector_store/chroma_db"
+    )
+
+    print(
+        f"Vector Store Created: {len(chunks)} chunks"
+    )
+
+    return vector_store
+
+
+if __name__ == "__main__":
+
+    create_vector_store()
