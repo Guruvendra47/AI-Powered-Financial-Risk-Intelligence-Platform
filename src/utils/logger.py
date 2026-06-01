d16029b3cb375c15da7c089e18c6e917d597cbf3
Guruvendra47 <Guruvendra47@gmail.com>
2026-06-01 04:42:54 -0400

    Update logger.py

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,10 +1,33 @@
-from loguru import logger
+import logging
+import os
 
-logger.add(
-    "logs/application.log",
-    rotation="10 MB",
-    retention="10 days"
-)
 
-def get_logger():
+def get_logger(name: str):
+
+    os.makedirs("logs", exist_ok=True)
+
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter(
+        "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+    )
+
+    # Console Handler
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+
+    # File Handler
+    file_handler = logging.FileHandler(
+        "logs/application.log"
+    )
+    file_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
+
     return logger
