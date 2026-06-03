5f5d05d72911b07549d6d4424ad087a5b227c9c0
Guruvendra47 <Guruvendra47@gmail.com>
2026-06-03 17:03:52 -0400

    Create risk_analysis.sql

---
+++ src/cloud/snowflake/sql/risk_analysis.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE TABLE GOLD.RISK_ANALYSIS (
+
+    complaint_id NUMBER,
+
+    risk_category STRING,
+
+    sentiment STRING,
+
+    complaint_summary STRING,
+
+    analysis_status STRING,
+
+    model_name STRING,
+
+    processed_timestamp TIMESTAMP
+);
