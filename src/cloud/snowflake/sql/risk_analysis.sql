230693f89dac9b9c601c75fcfb65f583c1ff0bbe
Guruvendra47 <Guruvendra47@gmail.com>
2026-06-03 17:04:12 -0400

    Update risk_analysis.sql

--- src/cloud/snowflake/sql/risk_analysis.sql
+++ src/cloud/snowflake/sql/risk_analysis.sql
@@ -1,16 +1,9 @@
 CREATE OR REPLACE TABLE GOLD.RISK_ANALYSIS (
-
     complaint_id NUMBER,
-
     risk_category STRING,
-
     sentiment STRING,
-
     complaint_summary STRING,
-
     analysis_status STRING,
-
     model_name STRING,
-
     processed_timestamp TIMESTAMP
 );
