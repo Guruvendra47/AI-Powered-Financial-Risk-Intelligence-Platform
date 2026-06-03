c94b3dae5dcd36b136c1a2978791e60c5164c82a
Guruvendra47 <Guruvendra47@gmail.com>
2026-06-03 15:15:33 -0400

    Update and rename 4-query-documents.py to retriever.py

---
+++ src/rag/retriever.py
@@ -0,0 +1,36 @@
+from dotenv import load_dotenv
+
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+
+from src.utils.logger import get_logger
+
+load_dotenv()
+
+logger = get_logger(__name__)
+
+
+def get_retriever():
+
+    embeddings = OpenAIEmbeddings(
+        model="text-embedding-3-small"
+    )
+
+    vector_store = Chroma(
+        collection_name="financial_risk_documents",
+        persist_directory="vector_store/chroma_db",
+        embedding_function=embeddings
+    )
+
+    retriever = vector_store.as_retriever(
+        search_type="similarity",
+        search_kwargs={
+            "k": 5
+        }
+    )
+
+    logger.info(
+        "Retriever initialized successfully."
+    )
+
+    return retriever
