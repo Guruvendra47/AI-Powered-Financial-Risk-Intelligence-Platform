45639fd9bc5812706b54e1ca994530f8d3c47e90
Guruvendra47 <Guruvendra47@gmail.com>
2026-06-03 16:54:16 -0400

    Update main.py

--- src/ai/main.py
+++ src/ai/main.py
@@ -1,1 +1,46 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
 
+from src.rag.rag_chain import build_rag_chain
+
+app = FastAPI(
+    title="Financial Risk Intelligence API",
+    version="1.0.0"
+)
+
+rag_chain = build_rag_chain()
+
+
+class QuestionRequest(BaseModel):
+    question: str
+
+
+@app.get("/")
+def health_check():
+
+    return {
+        "status": "healthy",
+        "application":
+        "Financial Risk Intelligence Platform"
+    }
+
+
+@app.post("/api/v1/ask")
+def ask_question(
+    request: QuestionRequest
+):
+
+    response = rag_chain.invoke(
+        {
+            "input":
+            request.question
+        }
+    )
+
+    return {
+        "question":
+        request.question,
+
+        "answer":
+        response["answer"]
+    }
